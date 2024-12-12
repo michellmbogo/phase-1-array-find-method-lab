d6cc628647483b4d50d413607f61962669b99b98
Michell Wambui Mbogo <mishie254@gmail.com>
2024-12-12 12:56:04 +0300

    find method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+// index.js or your implementation file
+function superbowlWin(record) {
+    // Find the first record with a result of "W"
+    const winRecord = record.find(game => game.result === "W");
+
+    // Return the year of the win, or undefined if no win is found
+    return winRecord ? winRecord.year : undefined;
+}
+
+// Export the function for use in other files
+module.exports = superbowlWin;
+
